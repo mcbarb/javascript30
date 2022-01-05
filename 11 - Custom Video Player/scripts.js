7a6ba35c13e7a2cf503d6f1200a41dd32a3bd6a2
Marcelo Castro Barbosa <celocb@gmail.com>
2022-01-05 11:06:32 -0300

    finished exercise 11

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -8,13 +8,68 @@ const ranges = player.querySelectorAll('.player__slider');
 const volume = player.querySelector('[name="volume"]');
 const playbackRate = player.querySelector('[name="playbackRate"]');
 
+let mousedown = false;
 
-console.log(player);
-console.log(video);
-console.log(progress);
-console.log(progressBar);
-console.log(toggle);
-console.log(skipButtons);
-console.log(ranges);
-console.log(volume);
-console.log(playbackRate);
+function initControls() {
+    volume.value = video.volume;
+    playbackRate.value = video.playbackRate;
+    progressBar.style.flexBasis = '0%';
+}
+
+function togglePlay() {
+    const state = video.paused ? 'play' : 'pause';
+    return video[state]();
+}
+
+function updateButton() {
+    const icon = this.paused ? '►' : '❚ ❚';
+    toggle.textContent = icon;
+}
+
+function handleRanges() {
+    console.log(parseFloat(this.value));
+    video[this.name] = parseFloat(this.value);
+}
+
+function skipAction() {
+    video.currentTime += parseFloat(this.dataset.skip);
+}
+
+function updateProgressBar() {
+    const percentage = video.currentTime / video.duration * 100;
+    progressBar.style.flexBasis = `${percentage}%`;
+}
+
+function scrub(e) {
+    const newTime = e.offsetX / progress.offsetWidth * video.duration;
+    video.currentTime = newTime;
+}
+
+video.addEventListener('click', togglePlay);
+video.addEventListener('play', updateButton);
+video.addEventListener('pause', updateButton);
+video.addEventListener('timeupdate',updateProgressBar);
+
+toggle.addEventListener('click', togglePlay);
+
+ranges.forEach(range => range.addEventListener('change',handleRanges));
+
+skipButtons.forEach(skipButton => skipButton.addEventListener('click',skipAction));
+
+progress.addEventListener('click', scrub);
+progress.addEventListener('mousemove', (e) => mousedown && scrub(e));
+progress.addEventListener('mousedown', () => mousedown = true);
+progress.addEventListener('mouseleave', (e) => {if (mousedown) {console.log('mouseout');mousedown = false;}});
+progress.addEventListener('mouseup', () => mousedown = false);
+
+initControls();
+
+// console.log(player);
+// console.log(video);
+// console.log(progress);
+// console.log(progressBar);
+// console.log(toggle);
+// console.log(skipButtons);
+// console.log(ranges);
+// console.log(volume);
+// console.log(playbackRate);
